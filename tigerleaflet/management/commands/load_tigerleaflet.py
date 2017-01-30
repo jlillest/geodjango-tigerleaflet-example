f159a1dc24387040cd813daa2827eae96bb4edd8
Jon Lillestolen <jlillest@gmail.com>
2017-01-29 20:37:35 -0500

    Pull in changes from tigerleaflet 0.2.1

--- tigerleaflet/management/commands/load_tigerleaflet.py
+++ tigerleaflet/management/commands/load_tigerleaflet.py
@@ -1,39 +1,18 @@
 import datetime
-import sys
-from optparse import make_option
 
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-
-try:
-    from django.contrib.gis.utils import LayerMapping
-except ImportError:
-    print("gdal is required")
-    sys.exit(1)
-
-DOWNLOADS_PATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    'TIGERLINE_DOWNLOADS'
-)
-# taken from https://github.com/victor-o-silva/django-brasil-municipios/blob/master/brasil_municipios/management/commands/loadmunicipios.py
-def download_from_census(filename):
-    folder = os.path.basename(filename).replace('.zip', '')
-    zip_file_path = os.path.join(DOWNLOADS_PATH, folder)
-
-    with open(zip_file_path, 'wb') as zip_file:
-        ftp = ftplib.FTP('ftp2.census.gov')
-        ftp.login('anonymous', 'anonymous')
-        ftp.cwd('geo/tiger/TIGER2016/STATE/tl_2016_us_state.zip
 
 
 class Command(BaseCommand):
     help = 'Installs the 2011-2016 TIGER/LINE files for states and counties'
 
     def add_arguments(self, parser):
+        help_string = 'The directory where the TIGER/LINE data is stored.'
         parser.add_argument('--path', default='', dest='path',
-            help='The directory where the TIGER/LINE data is stored.',
-        )
+                            help=help_string,
+                            )
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
